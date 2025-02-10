cbe0cd4f10ccd8f2ee549cd789b700d97f14900f
Шахмурат <onlymeand@gmail.com>
2025-02-10 20:15:07 +0500

    add linter, jest, makefile, fixtures and basic app code

--- Makefile
+++ Makefile
@@ -4,5 +4,11 @@ install:
 gendiff:
 	node bin/gendiff.js
 
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest
+
+lint:
+	npx eslint .
+
 publish:
 	npm publish --dry-run
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+import { Command } from 'commander';
+import genDiff from '../src/genDiff.js';
+
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format');
-  
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+
 program.parse();
