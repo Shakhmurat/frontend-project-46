c412ac74b30a4ee3a94d304cb64627bbfa4662aa
Шахмурат <onlymeand@gmail.com>
2025-02-18 23:07:34 +0500

    add github action, test and fixture

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,17 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import { dirname, join } from 'path';
 import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff', () => {
-  it('test 1', () => {
-    console.log(readFile('file1.json'));
-    // expect(actual).toBe(expected);
-  });
-
-  it('test 2', () => {
-    console.log(genDiff('file1.json', 'file2.json'));
+  it('json files without nested keys', () => {
+    const actual = genDiff('file1.json', 'file2.json');
+    const expected = readFile('expected.txt');
+    expect(actual).toBe(expected);
   });
 });
