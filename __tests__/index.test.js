c252c838ba3ff9e59eb5687df4a04713375b50f1
Шахмурат <onlymeand@gmail.com>
2025-03-13 15:29:10 +0500

    delete extra code from tests, fixtures and src/index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,61 +9,58 @@ const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filen
 const getFixtureContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff when format is stylish', () => {
+  const expected = getFixtureContent('expected-stylish.txt');
+
   it('json files with nested keys', () => {
     const actual = genDiff('file1.json', 'file2.json');
-    const expected = getFixtureContent('expected-stylish.txt');
     expect(actual).toBe(expected);
   });
 
   it('yaml and json with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.json');
-    const expected = getFixtureContent('expected-stylish.txt');
     expect(actual).toBe(expected);
   });
 
   it('yaml files with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.yaml');
-    const expected = getFixtureContent('expected-stylish.txt');
     expect(actual).toBe(expected);
   });
 });
 
 describe('genDiff when format is plain', () => {
+  const expected = getFixtureContent('expected-plain.txt');
+
   it('json files with nested keys', () => {
     const actual = genDiff('file1.json', 'file2.json', 'plain');
-    const expected = getFixtureContent('expected-plain.txt');
     expect(actual).toBe(expected);
   });
 
   it('yaml and json with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.json', 'plain');
-    const expected = getFixtureContent('expected-plain.txt');
     expect(actual).toBe(expected);
   });
 
   it('yaml files with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.yaml', 'plain');
-    const expected = getFixtureContent('expected-plain.txt');
     expect(actual).toBe(expected);
   });
 });
 
 describe('genDiff when format is json', () => {
+  const expected = getFixtureContent('expected-json.txt');
+
   it('json with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.yaml', 'json');
-    const expected = getFixtureContent('expected-json.txt');
     expect(actual).toBe(expected);
   });
 
   it('yaml and json with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.json', 'json');
-    const expected = getFixtureContent('expected-json.txt');
     expect(actual).toBe(expected);
   });
 
   it('yaml files with nested keys', () => {
     const actual = genDiff('file1.yml', 'file2.yaml', 'json');
-    const expected = getFixtureContent('expected-json.txt');
     expect(actual).toBe(expected);
   });
 });
